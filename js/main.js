d710232a99c41a32ce61aa35a10c95bc9259541e
= <vikytrusowa.@gmail.com>
2020-04-28 16:39:13 +0300

    add animate circle

--- js/main.js
+++ js/main.js
@@ -75,16 +75,24 @@ function getResults() {
 
 $(document).ready(function () {
 
+    setTimeout(function() {
+        $('.container-fluid').addClass('loaded');
+    }, 3000);
+    setTimeout(function() {
+        $('.circle').hide();
+    }, 5000);
+
     setTimeout(function () {
         $("#preloader").removeClass('preload__animate');
-        $(".loader").addClass('loader__hidden');
-    }, 1000);
+        $(".container-fluid").css('opacity',"0");
+    }, 7000);
     setTimeout(function () {
         $("#preloader").addClass('preload__animate');
         setTimeout(function () {
             $("#preloader").addClass('preload__hidden');
+            $(".container-fluid").addClass('preload__hidden');
         }, 1000);
-    }, 5000);
+    }, 12000);
 
 
 
